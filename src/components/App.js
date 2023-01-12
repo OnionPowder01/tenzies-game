3ee61cccde169aaabf47af9ffbe9a77871f2b6a0
OnionPowder01 <autonatione36@gmail.com>
2023-01-12 18:08:49 +0200

    Dice random value feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Die from './Die'
 
-export default function App(){ 
+export default function App() { 
 
 
 
+function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.floor(Math.random() * (6 - 1 + 1) + 1))
 
+     }
+     return newDice;
+}
 
-
-
+console.log(allNewDice())
 return(
     <main>
         <div className="dice--container">
-        <Die value="1"/>
+        <Die value='1'/>
         <Die value="2"/>
         <Die value="3"/>
         <Die value="4"/>
@@ -26,4 +32,5 @@ return(
     </main>
     
 )
+
 }
