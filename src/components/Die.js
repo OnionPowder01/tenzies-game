f0cd34732c784208f3e93d1f67e2eba0dfc17aa9
OnionPowder01 <autonatione36@gmail.com>
2023-01-14 09:07:14 +0200

    ternary styling

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 
+
 export default function Die(props) {
 
+const styles = {backgroundColor: props.isHeld ? '#59E391' : ''}
+
+console.log(props)
     return(
-        <div className="die--box">
+        <div 
+        className="die--box" 
+        style={styles}
+        onClick={() => props.holdDice(props.id)}
+        >
             <div className="die--value">{props.value}</div>
         </div>
     )
