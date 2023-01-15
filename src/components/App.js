5dd192798294c2d834fc9e5acf7ee3a266e3c73e
OnionPowder01 <autonatione36@gmail.com>
2023-01-15 18:04:01 +0200

    possibility to 'hold' another dice after unholding  old ones

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import Die from './Die'
 import { nanoid } from "nanoid"
 
@@ -6,6 +6,7 @@ export default function App() {
 
 const [dice, setDice] = useState(allNewDice())
 const [number, setNumber] = useState(0)
+const [count, setCount] = useState(0)
     
 function generateNewDie() {
     return {
@@ -46,19 +47,32 @@ function rollDice(){
        
     
 function holdDice(diceId, diceNumber) {
+    setCount(prevCount => prevCount + 1)
+
     if(number === 0) {
         firstDiceValue(diceId, diceNumber)
     }
+
     if(number === diceNumber || number === 0) {
+
     setDice(oldDice => oldDice.map(die => {
         return die.id === diceId ? 
         {...die, isHeld: !die.isHeld} :
         die
     }))
-    } 
+    }
+    
 }
 
-function firstDiceValue(diceNumber) {
+useEffect(() => {
+    if(dice.every((die) => die.isHeld === false)) {
+        setNumber(0)
+    }
+},[count])
+
+
+
+function firstDiceValue(diceID, diceNumber) {
     setNumber(diceNumber)
 }
 
@@ -72,6 +86,5 @@ return(
     </main>
     
 )
-
 
 }
